4c8101f689fda193268b891f05132cfb9e385483
mektigboy <mektigboy@protonmail.com>
2022-08-29 15:15:55 -0500

    Update dex folder

---
+++ dex/frontend/pages/index.js
@@ -0,0 +1,394 @@
+import Head from "next/head";
+import Web3Modal from "web3modal";
+import { addLiquidity, calculateCD } from "../utils/addLiquidity";
+import { BigNumber, providers, utils } from "ethers";
+import {
+  getCDTokensBalance,
+  getEtherBalance,
+  getLPTokensBalance,
+  getReserveOfCDTokens,
+} from "../utils/getAmounts";
+import {
+  getTokensAfterRemove,
+  removeLiquidity,
+} from "../utils/removeLiquidity";
+import { swapTokens, getAmountOfTokensReceivedFromSwap } from "../utils/swap";
+import { useEffect, useRef, useState } from "react";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  const [loading, setLoading] = useState(false);
+  const [liquidityTab, setLiquidityTab] = useState(true);
+
+  const zero = BigNumber.from(0);
+  const [ethBalance, setEtherBalance] = useState(zero);
+  const [reservedCD, setReservedCD] = useState(zero);
+  const [etherBalanceContract, setEtherBalanceContract] = useState(zero);
+  const [cdBalance, setCDBalance] = useState(zero);
+  const [lpBalance, setLPBalance] = useState(zero);
+  const [addEther, setAddEther] = useState(zero);
+  const [addCDTokens, setAddCDTokens] = useState(zero);
+  const [removeEther, setRemoveEther] = useState(zero);
+  const [removeCD, setRemoveCD] = useState(zero);
+  const [removeLPTokens, setRemoveLPTokens] = useState("0");
+  const [swapAmount, setSwapAmount] = useState("");
+  const [tokenToBeReceivedAfterSwap, settokenToBeReceivedAfterSwap] =
+    useState(zero);
+  const [ethSelected, setEthSelected] = useState(true);
+  const web3ModalRef = useRef();
+  const [walletConnected, setWalletConnected] = useState(false);
+
+  const getAmounts = async () => {
+    try {
+      const provider = await getProviderOrSigner(false);
+      const signer = await getProviderOrSigner(true);
+      const address = await signer.getAddress();
+      const _ethBalance = await getEtherBalance(provider, address);
+      const _cdBalance = await getCDTokensBalance(provider, address);
+      const _lpBalance = await getLPTokensBalance(provider, address);
+      const _reservedCD = await getReserveOfCDTokens(provider);
+      const _ethBalanceContract = await getEtherBalance(provider, null, true);
+
+      setEtherBalance(_ethBalance);
+      setCDBalance(_cdBalance);
+      setLPBalance(_lpBalance);
+      setReservedCD(_reservedCD);
+      setReservedCD(_reservedCD);
+      setEtherBalanceContract(_ethBalanceContract);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const _swapTokens = async () => {
+    try {
+      const swapAmountWei = utils.parseEther(swapAmount);
+      if (!swapAmountWei.eq(zero)) {
+        const signer = await getProviderOrSigner(true);
+
+        setLoading(true);
+        await swapTokens(
+          signer,
+          swapAmountWei,
+          tokenToBeReceivedAfterSwap,
+          ethSelected
+        );
+        setLoading(false);
+        await getAmounts();
+        setSwapAmount("");
+      }
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+      setSwapAmount("");
+    }
+  };
+
+  const _getAmountOfTokensReceivedFromSwap = async (_swapAmount) => {
+    try {
+      const _swapAmountWEI = utils.parseEther(_swapAmount.toString());
+      if (!_swapAmountWEI.eq(zero)) {
+        const provider = await getProviderOrSigner();
+        const _ethBalance = await getEtherBalance(provider, null, true);
+        const amountOfTokens = await getAmountOfTokensReceivedFromSwap(
+          _swapAmountWEI,
+          provider,
+          ethSelected,
+          _ethBalance,
+          reservedCD
+        );
+        settokenToBeReceivedAfterSwap(amountOfTokens);
+      } else {
+        settokenToBeReceivedAfterSwap(zero);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const _addLiquidity = async () => {
+    try {
+      const addEtherWei = utils.parseEther(addEther.toString());
+
+      if (!addCDTokens.eq(zero) && !addEtherWei.eq(zero)) {
+        const signer = await getProviderOrSigner(true);
+
+        setLoading(true);
+        await addLiquidity(signer, addCDTokens, addEtherWei);
+        setLoading(false);
+        setAddCDTokens(zero);
+        await getAmounts();
+      } else {
+        setAddCDTokens(zero);
+      }
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+      setAddCDTokens(zero);
+    }
+  };
+
+  const _removeLiquidity = async () => {
+    try {
+      const signer = await getProviderOrSigner(true);
+      const removeLPTokensWei = utils.parseEther(removeLPTokens);
+
+      setLoading(true);
+      await removeLiquidity(signer, removeLPTokensWei);
+      setLoading(false);
+      await getAmounts();
+      setRemoveCD(zero);
+      setRemoveEther(zero);
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+      setRemoveCD(zero);
+      setRemoveEther(zero);
+    }
+  };
+
+  const _getTokensAfterRemove = async (_removeLPTokens) => {
+    try {
+      const provider = await getProviderOrSigner();
+      const removeLPTokenWei = utils.parseEther(_removeLPTokens);
+      const _ethBalance = await getEtherBalance(provider, null, true);
+      const cryptoDevTokenReserve = await getReserveOfCDTokens(provider);
+      const { _removeEther, _removeCD } = await getTokensAfterRemove(
+        provider,
+        removeLPTokenWei,
+        _ethBalance,
+        cryptoDevTokenReserve
+      );
+
+      setRemoveEther(_removeEther);
+      setRemoveCD(_removeCD);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      await getProviderOrSigner();
+      setWalletConnected(true);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getProviderOrSigner = async (needSigner = false) => {
+    const provider = await web3ModalRef.current.connect();
+    const web3Provider = new providers.Web3Provider(provider);
+
+    const { chainId } = await web3Provider.getNetwork();
+
+    if (chainId !== 4) {
+      window.alert("Change the network to Rinkeby.");
+      throw new Error("Change network to Rinkeby.");
+    }
+
+    if (needSigner) {
+      const signer = web3Provider.getSigner();
+      return signer;
+    }
+    return web3Provider;
+  };
+
+  useEffect(() => {
+    if (!walletConnected) {
+      web3ModalRef.current = new Web3Modal({
+        network: "rinkeby",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+      connectWallet();
+      getAmounts();
+    }
+  }, [walletConnected]);
+
+  const renderButton = () => {
+    if (!walletConnected) {
+      return (
+        <button onClick={connectWallet} className={styles.button}>
+          Connect Wallet
+        </button>
+      );
+    }
+
+    if (loading) {
+      return <button className={styles.button}>Loading...</button>;
+    }
+
+    if (liquidityTab) {
+      return (
+        <div>
+          <div className={styles.description}>
+            You have:
+            <br />
+            {utils.formatEther(cdBalance)} Crypto Dev tokens
+            <br />
+            {utils.formatEther(ethBalance)} ETH
+            <br />
+            {utils.formatEther(lpBalance)} Crypto Dev LP tokens
+          </div>
+          <div>
+            {utils.parseEther(reservedCD.toString()).eq(zero) ? (
+              <div>
+                <input
+                  type="number"
+                  placeholder="Amount ETH"
+                  onChange={(event) => setAddEther(event.target.value || "0")}
+                  className={styles.input}
+                />
+                <input
+                  type="number"
+                  placeholder="Amount Crypto Dev tokens"
+                  onChange={(event) =>
+                    setAddCDTokens(
+                      BigNumber.from(
+                        utils.parseEther(event.target.value || "0")
+                      )
+                    )
+                  }
+                  className={styles.input}
+                />
+                <button className={styles.button1} onClick={_addLiquidity}>
+                  Add
+                </button>
+              </div>
+            ) : (
+              <div>
+                <input
+                  type="number"
+                  placeholder="Amount ETH"
+                  onChange={async (event) => {
+                    setAddEther(event.target.value || "0");
+                    const _addCDTokens = await calculateCD(
+                      event.target.value || "0",
+                      etherBalanceContract,
+                      reservedCD
+                    );
+                    setAddCDTokens(_addCDTokens);
+                  }}
+                  className={styles.input}
+                />
+                <div className={styles.inputDiv}>
+                  {`You will need ${utils.formatEther(addCDTokens)} Crypto Dev
+                  tokens.`}
+                </div>
+                <button className={styles.button1} onClick={_addLiquidity}>
+                  Add
+                </button>
+              </div>
+            )}
+            <div>
+              <input
+                type="number"
+                placeholder="Amount LP Tokens"
+                onChange={async (event) => {
+                  setRemoveLPTokens(event.target.value || "0");
+                  await _getTokensAfterRemove(event.target.value || "0");
+                }}
+                className={styles.input}
+              />
+              <div className={styles.inputDiv}>
+                {`You will get ${utils.formatEther(removeCD)} Crypto
+              Dev Tokens and ${utils.formatEther(removeEther)} Eth`}
+              </div>
+              <button className={styles.button1} onClick={_removeLiquidity}>
+                Remove
+              </button>
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <input
+            type="number"
+            placeholder="Amount"
+            onChange={async (event) => {
+              setSwapAmount(event.target.value || "");
+              await _getAmountOfTokensReceivedFromSwap(
+                event.target.value || "0"
+              );
+            }}
+            className={styles.input}
+            value={swapAmount}
+          />
+          <select
+            className={styles.select}
+            name="dropdown"
+            id="dropdown"
+            onChange={async () => {
+              setEthSelected(!ethSelected);
+              await _getAmountOfTokensReceivedFromSwap(0);
+              setSwapAmount("");
+            }}
+          >
+            <option value="eth">Ethereum</option>
+            <option value="cryptoDevToken">Crypto Dev Token</option>
+          </select>
+          <br />
+          <div className={styles.inputDiv}>
+            {ethSelected
+              ? `You will get ${utils.formatEther(
+                  tokenToBeReceivedAfterSwap
+                )} Crypto Dev Tokens`
+              : `You will get ${utils.formatEther(
+                  tokenToBeReceivedAfterSwap
+                )} Eth`}
+          </div>
+          <button className={styles.button1} onClick={_swapTokens}>
+            Swap
+          </button>
+        </div>
+      );
+    }
+  };
+
+  return (
+    <div>
+      <Head>
+        <title>Crypto Devs</title>
+        <meta name="description" content="Whitelist-Dapp" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={styles.main}>
+        <div>
+          <h1 className={styles.title}>Welcome to Crypto Devs Exchange!</h1>
+          <div className={styles.description}>
+            Exchange Ethereum &#60;&#62; Crypto Dev Tokens
+          </div>
+          <div>
+            <button
+              className={styles.button}
+              onClick={() => {
+                setLiquidityTab(true);
+              }}
+            >
+              Liquidity
+            </button>
+            <button
+              className={styles.button}
+              onClick={() => {
+                setLiquidityTab(false);
+              }}
+            >
+              Swap
+            </button>
+          </div>
+          {renderButton()}
+        </div>
+        <div>
+          <img className={styles.image} src="./cryptodev.svg" />
+        </div>
+      </div>
+
+      <footer className={styles.footer}>
+        Made with &#10084; by Crypto Devs.
+      </footer>
+    </div>
+  );
+}
