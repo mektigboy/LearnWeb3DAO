75b06729718ae509f4de25374890fd8d02f4c2ae
mektigboy <mektigboy@protonmail.com>
2022-08-28 20:45:04 -0500

    Update nft/hardhat.config.js

--- nft/hardhat.config.js
+++ nft/hardhat.config.js
@@ -5,7 +5,7 @@ const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.8.15",
   networks: {
     rinkeby: {
       url: ALCHEMY_API_KEY_URL,
